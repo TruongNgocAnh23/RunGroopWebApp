4095812dcff732af87c63aa91c4fffc968d007e7
TruongNgocAnh23 <ngocanhdip@gmail.com>
2024-02-20 02:29:55 +0700

    add controller

--- RunGroopWebApp/obj/Debug/net6.0/RunGroopWebApp.AssemblyInfo.cs
+++ RunGroopWebApp/obj/Debug/net6.0/RunGroopWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunGroopWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9b777cfd46195627f35d52bb1ebaf5c7190f1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunGroopWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RunGroopWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
